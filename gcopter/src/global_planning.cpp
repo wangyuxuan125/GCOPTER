f3d56180b0e6c97bc365b9f5b186007c7a9b602b
wangyuxuan125 <1097546466@qq.com>
2026-09-06 11:09:40 +0800

    Revert "Add clean final trajectory objective metric"
    This reverts commit b3450343ea39fa6b6322352538503051a12a7efb.

--- gcopter/src/global_planning.cpp
+++ gcopter/src/global_planning.cpp
@@ -107,7 +107,6 @@ struct Config
     double decompLocalBBoxVertical;
     double decompMaxSegmentLength;
     double decompMinOverlapRadius;
-    bool runBaselineFiriBenchmark = false;
 
     Config(const ros::NodeHandle &nh_priv)
     {
@@ -164,11 +163,7 @@ struct Config
             "Benchmark/Method",
             benchmarkMethod,
             "proposed");
-        nh_priv.param(
-            "Benchmark/RunBaselineFiri",
-            runBaselineFiriBenchmark,
-            false);
-
+        
         nh_priv.param<std::string>(
             "Benchmark/Variant",
             benchmarkVariant,
@@ -3152,22 +3147,22 @@ public:
                     const double finalTrajectoryDuration =
                         hardProjectedTrajectory
                             .getTotalDuration();
-
+                                    
                     const double finalSmoothnessEnergy =
                         hardProjectionResult
                             .final_energy;
-
+                                    
                     const double finalTimeWeight =
                         config.weightT;
-
+                                    
                     const double finalTimeCost =
                         finalTimeWeight *
                         finalTrajectoryDuration;
-
+                                    
                     const double finalJkin =
                         finalSmoothnessEnergy +
                         finalTimeCost;
-
+                                    
                     const bool finalJkinValid =
                         std::isfinite(
                             finalTrajectoryDuration) &&
